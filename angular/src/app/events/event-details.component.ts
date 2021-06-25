6b016f27a6255d4741176fb84be4d59ecc1fc680
Modafferi <gennaro.modafferi@capgemini.com>
2021-06-25 23:10:00 +0200

    add jquery import

--- angular/src/app/events/event-details.component.ts
+++ angular/src/app/events/event-details.component.ts
@@ -2,38 +2,16 @@ import { Component} from "@angular/core";
 
 @Component({
     selector:'event-details',
-    template:
-    `
-    <div>
-    <h1>Event details component</h1>
-  
-    <div style="margin-top:30px">
-      Event: {{event.name}}
-    </div>
-    <div>
-      Date: {{event.date}}
-    </div>
-    <div>
-      Time: {{event.time}}
-    </div>
-    <div>
-      Price: \€{{event.price}}
-    </div>
-    <div>
-      Address: {{event.location.address}}, {{event.location.city}}, {{event.location.country}}
-      </div>
-  </div>
-    
-    `
+    templateUrl:'./event-details.component.html'
 })
 
 export class EventDetailsComponent{
-    event =
+     event =
       {
          name:'ngConf 2025',
           date: '3/1/2025',
            time: '8am',
-           price : '124',
+           price: '124',
             location:
              {
                  address: '123 Main St',
