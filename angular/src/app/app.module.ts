d306ea20b6089a664cd55654473f65706c22b0b2
Modafferi <gennaro.modafferi@capgemini.com>
2021-07-05 10:56:58 +0200

    add service in order to split reusable service with component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrService } from './common/toastr.service';
 import { EventAppComponent } from './events-app.component';
 import { EventDetailsComponent } from './events/event-details.component';
 import { EventListComponent } from './events/event-list.component';
 import { NavBarComponent } from './nav/mavbar.component';
+import { EventService } from './shared/event.service';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,7 @@ import { NavBarComponent } from './nav/mavbar.component';
   imports: [
     BrowserModule,
   ],
-  providers: [],
+  providers: [EventService,ToastrService],
   //First component call when app starting 
   bootstrap: [EventAppComponent]
 })
