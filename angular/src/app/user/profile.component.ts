03425f3209ea6364fb7fbdd13b30e83b9ab74ee3
Modafferi <gennaro.modafferi@capgemini.com>
2021-07-14 22:45:52 +0200

    add profile component

--- angular/src/app/user/profile.component.ts
+++ angular/src/app/user/profile.component.ts
@@ -1,31 +1,48 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
 import { AuthService } from './auth.service'
 
 @Component({
   templateUrl: './profile.component.html',
+  styles: [
+    `
+    em {float:right; color#E05C65; padding-left:10px;}
+     .error input {background-color : #E3C3C5;}
+     .error ::-webkit-input-placeholder {color :#999;}
+     .error ::-moz-placeholder { color: #999;}
+     .error :-moz-placeholder {color :#999;}
+     .error :ms-input-placeholder {color : #999;} 
+  `]
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup
 
-  constructor(private authService: AuthService, private router:Router) {
+
+  constructor(private authService: AuthService, private route: Router) {
 
   }
 
   ngOnInit() {
-    let firstName = new FormControl(this.authService.currentUser.firstname)
-    let lastName = new FormControl(this.authService.currentUser.lastname)
+    let firstName = new FormControl(this.authService.currentUser.firstname, Validators.required);
+    let lastName = new FormControl(this.authService.currentUser.lastname, Validators.required);
     this.profileForm = new FormGroup({
       firstName: firstName,
       lastName: lastName
     })
   }
-    cancel(){
-      this.router.navigate(['events'])
-    }
 
-    saveProfile(values){
-      this.authService.updateUserInfo(values.firstName,values.lastName)
+  cancel() {
+    this.route.navigate(['events'])
+  }
+  saveProfile(formValues) {
+    if (this.profileForm.valid) {
+      this.authService.updateUserInfo(formValues.firstName, formValues.lastName)
+      this.route.navigate(['events'])
     }
+  }
+  isInvalid(){
+    return this.profileForm.controls.firstName.invalid &&
+    this.profileForm.controls.firstName.touched;
+  }
 }
